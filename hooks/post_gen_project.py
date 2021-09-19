8a4cf9d0b5399562bdd171e2c3d3deb0273da854
Lie Ryan <lie.1296@gmail.com>
2021-09-19 16:30:26 +1000

    Add basic ISSUE_TEMPLATE.md

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 import os
+import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
 if __name__ == '__main__':
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
+
+    if 'Do not set up repository' == '{{ cookiecutter.repository_type }}':
+        remove_file('.github')
